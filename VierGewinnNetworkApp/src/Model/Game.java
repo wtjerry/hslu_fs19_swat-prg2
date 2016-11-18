9f99be13d1903cb24759d872168fef4d0f1f27b3
Meledor <michaelzimmermann1@hotmail.com>
2016-11-18 15:11:42 +0100

    Base

---
+++ VierGewinnNetworkApp/src/Model/Game.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class Game {
+	
+	private String name;
+	private int[] score;
+	private Player Player1;
+	private Player Player2;
+    
+	public Game(){
+		
+	}
+
+}
--- VierGewinnNetworkApp/src/Views/MainUI.java
+++ VierGewinnNetworkApp/src/Views/MainUI.java
@@ -1,3 +1,4 @@
+package Views;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
