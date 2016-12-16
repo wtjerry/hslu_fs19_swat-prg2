7863987bb70ad621484090f09ebd6c4840ec3e98
wtjerry <wtjerry123@gmail.com>
2016-12-16 01:28:18 +0100

    dummy implementation of NetworkViewPanel

--- VierGewinnNetworkApp/src/Views/subPanel/NetworkViewPanel.java
+++ VierGewinnNetworkApp/src/Views/subPanel/NetworkViewPanel.java
@@ -8,10 +8,12 @@ package Views.subPanel;
 import Views.Interfaces.NetworkView;
 import Views.Interfaces.NetworkViewListener;
 import java.util.List;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
 
 
 public class NetworkViewPanel extends JPanel implements NetworkView{
@@ -19,15 +21,17 @@ public class NetworkViewPanel extends JPanel implements NetworkView{
     private NetworkViewListener networkViewListener;
     
     public NetworkViewPanel(){
+        this.computerListModel = new DefaultListModel();
         initComponent();
     }
     
     protected final void initComponent(){
         listScrollPane = new JScrollPane();
-        computerList = new JList<>();
+        computerList = new JList<>(this.computerListModel);
         back = new JButton("Back");
         startGame = new JButton("Start Game");
         
+        computerList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         listScrollPane.setViewportView(computerList);
         
         back.addActionListener(x -> {
@@ -36,9 +40,9 @@ public class NetworkViewPanel extends JPanel implements NetworkView{
             }
         });
         
-        String selectedIpAddress = "192.168.1.8";
         startGame.addActionListener(x -> {
             if (this.networkViewListener != null) {
+                String selectedIpAddress = this.computerList.getSelectedValue();
                 this.networkViewListener.StartGamePressed(selectedIpAddress);
             }
         });
@@ -49,9 +53,11 @@ public class NetworkViewPanel extends JPanel implements NetworkView{
     }
     @Override
     public void showAvailablePlayers(List<String> allPlayers) {
-        //todo implement show available players
+        this.computerListModel.removeAllElements();
+        allPlayers.forEach(player -> this.computerListModel.addElement(player));
     }
     
+    private final DefaultListModel computerListModel;
     private JScrollPane listScrollPane;
     private JList<String> computerList;
     private JButton startGame;
