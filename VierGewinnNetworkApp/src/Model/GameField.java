99eff859440026f042e2429b5ae578ead7898175
wtjerry <wtjerry123@gmail.com>
2016-12-19 22:35:06 +0100

    finished refactoring GameField

--- VierGewinnNetworkApp/src/Model/GameField.java
+++ VierGewinnNetworkApp/src/Model/GameField.java
@@ -1,7 +1,6 @@
 package Model;
 
 import Model.Network.Settings;
-import java.util.ArrayList;
 
 public class GameField implements ValidTurnChecker {
 
@@ -48,65 +47,35 @@ public class GameField implements ValidTurnChecker {
 
         return DiskPosition.getInvalidDiskPosition();
     }
-
-    public WinState checkIfSomebodyWon() {
-        WinState result = WinState.NobodyWon;
-        for (int i : this.getNumbers()) {
-            if (i == this.myDiskId * 4) {
-                result = WinState.IWon;
-            }
-            if (i == this.opponentDiskId * 4) {
-                result = WinState.OpponentWon;
-            }
-        }
-        return result;
-    }
     
-    public ArrayList<Integer> getNumbers() {
-        ArrayList<Integer> numbers = new ArrayList<>();
-
+    public WinState checkIfSomebodyWon() {
         //starting bottom left corner. left to right and bottom to top.
         for (int row = 0; row < this.height; row++) {
             for (int column = 0; column < this.width; column++) {
-                if (this.diskHasEnoughNeighboursToTheRightToPotentiallyHave4InARow(column)) {
-                    int sumOfNext4DiskIdsToTheRight = 0;
-                    for (int currentXOffsetToTheRight = 0; currentXOffsetToTheRight < NumberOfStonesToWin; currentXOffsetToTheRight++) {
-                        final int index = (column + currentXOffsetToTheRight) + (row * this.width);
-                        sumOfNext4DiskIdsToTheRight += this.gameArray[index];
-                    }
-                    numbers.add(sumOfNext4DiskIdsToTheRight);
+                
+                WinState xDisksInARowToTheRightWinResult = this.hasSomebodyXDisksInARowToTheRight(column, row);
+                if (xDisksInARowToTheRightWinResult != WinState.NobodyWon) {
+                    return xDisksInARowToTheRightWinResult;
                 }
                 
-                if (this.diskHasEnoughNeighboursToTheTopToPotentiallyHave4InARow(row)) {
-                    int sumOfNext4DiskIdsToTheTop = 0;
-                    for (int currentYOffsetToTheTop = 0; currentYOffsetToTheTop < NumberOfStonesToWin; currentYOffsetToTheTop++) {
-                        final int index = column + ((row + currentYOffsetToTheTop) * this.width);
-                        sumOfNext4DiskIdsToTheTop += this.gameArray[index];
-                    }
-                    numbers.add(sumOfNext4DiskIdsToTheTop);
+                WinState xDisksInARowToTheTopWinResult = this.hasSomebodyXDisksInARowToTheTop(column, row);
+                if (xDisksInARowToTheTopWinResult != WinState.NobodyWon) {
+                    return xDisksInARowToTheTopWinResult;
                 }
                 
-                if (this.diskHasEnoughNeighboursToTheTopRightToPotentiallyHave4InARow(column, row)) {
-                    int sumOfNext4DiskIdsToTheTopRight = 0;
-                    for (int currentXYOffsetToTheTopRight = 0; currentXYOffsetToTheTopRight < NumberOfStonesToWin; currentXYOffsetToTheTopRight++) {
-                        final int index = (column + currentXYOffsetToTheTopRight) + ((row + currentXYOffsetToTheTopRight) * this.width);
-                        sumOfNext4DiskIdsToTheTopRight += this.gameArray[index];
-                    }
-                    numbers.add(sumOfNext4DiskIdsToTheTopRight);
+                WinState xDisksInARowToTheTopRightWinResult = this.hasSomebodyXDisksInARowToTheTopRight(column, row);
+                if (xDisksInARowToTheTopRightWinResult != WinState.NobodyWon) {
+                    return xDisksInARowToTheTopRightWinResult;
                 }
                 
-                if (this.diskHasEnoughNeighboursToTheTopLeftToPotentiallyHave4InARow(column, row)) {
-                    int sumOfNext4DiskIdsToTheTopLeft = 0;
-                    for (int currentXYOffsetToTheTopLeft = 0; currentXYOffsetToTheTopLeft < NumberOfStonesToWin; currentXYOffsetToTheTopLeft++) {
-                        final int index = (column - currentXYOffsetToTheTopLeft) + ((row + currentXYOffsetToTheTopLeft) * this.width);
-                        sumOfNext4DiskIdsToTheTopLeft += this.gameArray[index];
-                    }
-                    numbers.add(sumOfNext4DiskIdsToTheTopLeft);
+                WinState xDisksInARowToTheTopLeftWinResult = this.hasSomebodyXDisksInARowToTheTopLeft(column, row);
+                if (xDisksInARowToTheTopLeftWinResult != WinState.NobodyWon) {
+                    return xDisksInARowToTheTopLeftWinResult;
                 }
             }
         }
 
-        return numbers;
+        return WinState.NobodyWon;
     }
 
     @Override
@@ -118,21 +87,85 @@ public class GameField implements ValidTurnChecker {
         return isStillFree;
     }
 
+    private WinState hasSomebodyXDisksInARowToTheRight(int column, int row) {
+        if (this.diskHasEnoughNeighboursToTheRightToPotentiallyHave4InARow(column)) {
+            int sumOfNext4DiskIdsToTheRight = 0;
+            for (int currentXOffsetToTheRight = 0; currentXOffsetToTheRight < NumberOfStonesToWin; currentXOffsetToTheRight++) {
+                final int index = (column + currentXOffsetToTheRight) + (row * this.width);
+                sumOfNext4DiskIdsToTheRight += this.gameArray[index];
+            }
+            return this.areThoseXDisksOfTheSamePlayer(sumOfNext4DiskIdsToTheRight);
+        }
+
+        return WinState.NobodyWon;
+    }
+    
     private boolean diskHasEnoughNeighboursToTheRightToPotentiallyHave4InARow(int column) {
         return (column + NumberOfStonesToWin) <= this.width;
     }
 
+    private WinState hasSomebodyXDisksInARowToTheTop(int column, int row) {
+        if (this.diskHasEnoughNeighboursToTheTopToPotentiallyHave4InARow(row)) {
+            int sumOfNext4DiskIdsToTheTop = 0;
+            for (int currentYOffsetToTheTop = 0; currentYOffsetToTheTop < NumberOfStonesToWin; currentYOffsetToTheTop++) {
+                final int index = column + ((row + currentYOffsetToTheTop) * this.width);
+                sumOfNext4DiskIdsToTheTop += this.gameArray[index];
+            }
+            return this.areThoseXDisksOfTheSamePlayer(sumOfNext4DiskIdsToTheTop);
+        }
+
+        return WinState.NobodyWon;
+    }
+    
     private boolean diskHasEnoughNeighboursToTheTopToPotentiallyHave4InARow(int row) {
         return (row + NumberOfStonesToWin) <= this.height;
     }
 
+    private WinState hasSomebodyXDisksInARowToTheTopRight(int column, int row) {
+        if (this.diskHasEnoughNeighboursToTheTopRightToPotentiallyHave4InARow(column, row)) {
+            int sumOfNext4DiskIdsToTheTopRight = 0;
+            for (int currentXYOffsetToTheTopRight = 0; currentXYOffsetToTheTopRight < NumberOfStonesToWin; currentXYOffsetToTheTopRight++) {
+                final int index = (column + currentXYOffsetToTheTopRight) + ((row + currentXYOffsetToTheTopRight) * this.width);
+                sumOfNext4DiskIdsToTheTopRight += this.gameArray[index];
+            }
+            return this.areThoseXDisksOfTheSamePlayer(sumOfNext4DiskIdsToTheTopRight);
+        }
+
+        return WinState.NobodyWon;
+    }
+    
     private boolean diskHasEnoughNeighboursToTheTopRightToPotentiallyHave4InARow(int column, int row) {
         return this.diskHasEnoughNeighboursToTheRightToPotentiallyHave4InARow(column) 
                 && this.diskHasEnoughNeighboursToTheTopToPotentiallyHave4InARow(row);
     }
 
+    private WinState hasSomebodyXDisksInARowToTheTopLeft(int column, int row) {
+        if (this.diskHasEnoughNeighboursToTheTopLeftToPotentiallyHave4InARow(column, row)) {
+            int sumOfNext4DiskIdsToTheTopLeft = 0;
+            for (int currentXYOffsetToTheTopLeft = 0; currentXYOffsetToTheTopLeft < NumberOfStonesToWin; currentXYOffsetToTheTopLeft++) {
+                final int index = (column - currentXYOffsetToTheTopLeft) + ((row + currentXYOffsetToTheTopLeft) * this.width);
+                sumOfNext4DiskIdsToTheTopLeft += this.gameArray[index];
+            }
+            return this.areThoseXDisksOfTheSamePlayer(sumOfNext4DiskIdsToTheTopLeft);
+        }
+        
+        return WinState.NobodyWon;
+    }
+    
     private boolean diskHasEnoughNeighboursToTheTopLeftToPotentiallyHave4InARow(int column, int row) {
         return (column + 1 - NumberOfStonesToWin) >= 0
                 && this.diskHasEnoughNeighboursToTheTopToPotentiallyHave4InARow(row);
+    }
+    
+    private WinState areThoseXDisksOfTheSamePlayer(int sum) {
+        WinState result = WinState.NobodyWon;
+        if (sum == this.myDiskId * NumberOfStonesToWin) {
+            result = WinState.IWon;
+        }
+        if (sum == this.opponentDiskId * NumberOfStonesToWin) {
+            result = WinState.OpponentWon;
+        }
+
+        return result;
     }
 }
